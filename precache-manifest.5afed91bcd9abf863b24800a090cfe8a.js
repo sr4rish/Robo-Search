60c912a868b377634d5a4623ab633392dd40440c
Sourav Rishishwar <36961707+sr4rish@users.noreply.github.com>
2020-05-03 00:12:49 +0530

    Updates

---
+++ precache-manifest.5afed91bcd9abf863b24800a090cfe8a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ae86d7a81544c3a827d3a9ec8b4220e",
+    "url": "/Robo-Search/index.html"
+  },
+  {
+    "revision": "84ca485f6fb5f1978a9f",
+    "url": "/Robo-Search/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "dc38910abba0c11067ac",
+    "url": "/Robo-Search/static/css/main.ed9bccfd.chunk.css"
+  },
+  {
+    "revision": "84ca485f6fb5f1978a9f",
+    "url": "/Robo-Search/static/js/2.12e19136.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robo-Search/static/js/2.12e19136.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc38910abba0c11067ac",
+    "url": "/Robo-Search/static/js/main.787e2b50.chunk.js"
+  },
+  {
+    "revision": "258e2e5038927de32640",
+    "url": "/Robo-Search/static/js/runtime-main.e21ea030.js"
+  },
+  {
+    "revision": "bac87289622939df6070372897e62592",
+    "url": "/Robo-Search/static/media/GeneralProductsRegular-d62Z.bac87289.woff"
+  }
+]);
